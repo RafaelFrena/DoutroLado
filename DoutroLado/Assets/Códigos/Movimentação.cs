4365a1a27dd595adbdf11b8cea57909abc6de527
RafaelFrena <rfaelfrena@gmail.com>
2021-06-04 14:23:34 -0300

    Movimentação e Animações
    -Problema dos pulos infinitos corrigido;
    -Regras e parâmetros das animações corrigidas (com exceção do pulo).

--- DoutroLado/Assets/Códigos/Movimentação.cs
+++ DoutroLado/Assets/Códigos/Movimentação.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class Movimentação : MonoBehaviour{
 
     public Rigidbody2D corpo;
+    public Animator animator;
+
+    //andar
     public int velocidade;
     private float sentido;
     private float direção;
+    
+    //pular
     public float forcapulo;
-    public Animator animator;
+
+    //detector do chao
+    public Transform detectorChao;
+    public LayerMask chao;
+    private bool noChao;
 
     // Start is called before the first frame update
     void Start(){
@@ -23,13 +32,14 @@ public class Movimentação : MonoBehaviour{
 
       //ANDAR PARA OS LADOS
       sentido = Input.GetAxis("Horizontal");
+      noChao = Physics2D.Linecast(transform.position, detectorChao.position, chao);
       corpo.velocity = new Vector2(sentido*velocidade, corpo.velocity.y);
-      animator.SetFloat("Velocidade", Mathf.Abs(sentido)); //animar ariel-pula
+      animator.SetFloat("Velocidade", Mathf.Abs(sentido)); //animar ariel-anda
+      
       //PULAR
-      if(Input.GetKeyDown(KeyCode.Space)){
+      if(Input.GetKeyDown(KeyCode.Space) && noChao){
         corpo.velocity = Vector2.up*forcapulo;
         animator.SetBool("Pulando", true); //animar ariel-pula
-        //(to do: estabeler o "landing" da ariel no tileset)
       }
 
     }
