298c0a50de53b36f3088a8a3004799e4ce68f961
RafaelFrena <rfaelfrena@gmail.com>
2021-07-12 10:57:13 -0300

    Movimentação e Animações v4
    -Código de controle da câmera adicionado (ainda com erros).

---
+++ DoutroLado/Assets/Códigos/CameraScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private GameObject jogador;
+    public float xMaximo;
+    public float xMinimo;
+    public float yMaximo;
+    public float yMinimo;
+
+    // Start is called before the first frame update
+    void Start(){
+      jogador = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate(){
+      float x = Mathf.Clamp(jogador.transform.position.x, xMinimo, xMaximo);
+      float y = Mathf.Clamp(jogador.transform.position.y, yMinimo, yMaximo);
+      gameObject.transform.position = new Vector3(x, y, GameObject.transform.position.z);
+    }
+}
