b01669f0eb2e9b4ea533d28f5ff581599b667d44
pedroFukis <87316790+pedroFukis@users.noreply.github.com>
2021-08-10 14:36:13 -0300

    Plataforma se movendo

---
+++ DoutroLado/Assets/Códigos/MovePlataforma.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlataforma : MonoBehaviour
+{
+
+    public GameObject plataforma;
+    public GameObject[] pontos;
+
+    public float velocidade = 5f;
+    public float espera = 1f;
+
+    public bool loop = true;
+
+    public Transform transform;
+    int i = 0;
+    float proxTempo;
+    bool seMovendo = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        transform = plataforma.transform;
+        proxTempo = 0f;
+        seMovendo = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Time.time >= proxTempo)
+        {
+            movimento();
+        }
+    }
+
+    void movimento()
+    {
+        if ((pontos.Length != 0) && seMovendo)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pontos[i].transform.position, velocidade * Time.deltaTime);
+        }
+        if (Vector3.Distance(pontos[i].transform.position, transform.position) < 0.0001f)
+        {
+            i++;
+            proxTempo = Time.time + espera;
+        }
+        if (i >= pontos.Length)
+        {
+            if (loop)
+            {
+                i = 0;
+            }
+            else
+            {
+                seMovendo = false;
+            }
+        }
+    }
+}
