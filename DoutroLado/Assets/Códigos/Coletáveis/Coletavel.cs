04c40672b2a121e045bfe92feac2ce0f3f0e1f73
RafaelFrena <rfaelfrena@gmail.com>
2021-08-13 01:39:46 -0300

    Reorganização

---
+++ DoutroLado/Assets/Códigos/Coletáveis/Coletavel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coletavel : MonoBehaviour{
+
+  public int valorObjeto = 1;
+  void OnTriggerEnter2D(Collider2D outroObjeto){
+    if(outroObjeto.gameObject.CompareTag("Player")){
+      ControladorPontuacao.instance.AtualizaPontuacao(valorObjeto);
+      Destroy(gameObject);
+    }
+  }
+
+}
---
+++ DoutroLado/Assets/Códigos/Dialogos/ControladorDialogo.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorDialogo : MonoBehaviour{
+
+    public Text nomeText;
+    public Text dialogoText;
+    public Animator animator;
+    public Queue<string> falas;
+
+    // Start is called before the first frame update
+    void Start(){
+      falas = new Queue<string>();
+    }
+
+    void Update(){
+
+      if(Input.GetKeyDown(KeyCode.X)){
+
+        MostrarProximaFala();
+
+      }
+
+      if(Input.GetKeyDown(KeyCode.Z)){
+
+        TerminarDialogo();
+
+      }
+
+    }
+
+    public void IniciarDialogo(Dialogo dialogo){
+
+      animator.SetBool("estaAberto", true);
+      nomeText.text = dialogo.nome;
+      falas.Clear();
+
+      foreach(string fala in dialogo.falas){
+        falas.Enqueue(fala);
+      }
+
+      MostrarProximaFala();
+
+    }
+
+    public void MostrarProximaFala(){
+      if(falas.Count == 0){
+        TerminarDialogo();
+        return;
+      }
+
+      string fala = falas.Dequeue();
+      StopAllCoroutines();
+      StartCoroutine(EscreverFala(fala));
+    }
+
+    IEnumerator EscreverFala(string fala){
+      dialogoText.text = "";
+      foreach (char letra in fala.ToCharArray()){
+        dialogoText.text += letra;
+        yield return null;
+      }
+
+    }
+
+    public void TerminarDialogo(){
+      animator.SetBool("estaAberto", false);
+
+      Movimentação movimentação = GameObject.FindGameObjectWithTag("Player").GetComponent<Movimentação>();
+      movimentação.podeSeMover = true;
+    }
+
+}
